926d14cd56f3c730afc2156986d42814162aa565
Tash <tasm86688@gmail.com>
2024-06-06 12:52:31 +0300

    Supabase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:velocio/src/common/di/injector.dart';
 import 'package:velocio/src/features/app/pages/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseKey = dotenv.env['SUPABASE_KEY'];
+
+  await Supabase.initialize(
+    url: supabaseUrl!,
+    anonKey: supabaseKey!,
+  );
 
   injectDependencies();
 
